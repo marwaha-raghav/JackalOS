bfece83abe7f59e874b08339caae57a7b3629099
AbhinavAtrishi <abhinav_avm@yahoo.co.in>
2019-03-27 19:31:44 +0530

    Improved mouse movement and major GUI revamp.

--- CosmosKernel1/CosmosKernel1/GUI.cs
+++ CosmosKernel1/CosmosKernel1/GUI.cs
@@ -10,150 +10,103 @@ public class GUI
     Canvas C;
     readonly int ScreenWidth = 800;
     readonly int ScreenHeight = 600;
+    readonly Pen MousePen = new Pen(Color.Black);
+    readonly Pen GUIHomePen = new Pen(Color.White);
+    Point PrevousMouse;
     public GUI()
 	{
         
     }
     public void Initialize() //To Initialize the Canvas and Mouse
     {
+        Console.WriteLine("GUI is booting up.");
         C = FullScreenCanvas.GetFullScreenCanvas(new Mode(ScreenWidth, ScreenHeight, ColorDepth.ColorDepth32));
+        C.Clear(Color.White);
         CMouse.ScreenWidth = (uint)ScreenWidth;
         CMouse.ScreenHeight = (uint)ScreenHeight;
-        CMouse.X = 400; //Initializing Mouse at the center of the screen
-        CMouse.Y = 300;
+        CMouse.X = (uint)(ScreenWidth / 2); //Initializing Mouse at the center of the screen
+        CMouse.Y = (uint)(ScreenHeight / 2);
+        PrevousMouse.X = (ScreenWidth / 2);
+        PrevousMouse.Y = (ScreenHeight / 2);
+        Console.WriteLine("GUI booted up successfully.");
+    }
+    public void DrawMouse(Pen pen, Point cur)
+    {
+        //Mouse Traingle Points are Below
+        C.DrawPoint(pen, cur.X, cur.Y);
+        C.DrawPoint(pen, new Point(cur.X + 1, cur.Y));
+        C.DrawPoint(pen, new Point(cur.X + 2, cur.Y));
+        C.DrawPoint(pen, new Point(cur.X + 3, cur.Y));
+        C.DrawPoint(pen, new Point(cur.X + 4, cur.Y));
+        C.DrawPoint(pen, new Point(cur.X + 5, cur.Y));
+
+        C.DrawPoint(pen, new Point(cur.X, cur.Y + 1));
+        C.DrawPoint(pen, new Point(cur.X + 1, cur.Y + 1));
+        C.DrawPoint(pen, new Point(cur.X + 2, cur.Y + 1));
+        C.DrawPoint(pen, new Point(cur.X + 3, cur.Y + 1));
+        C.DrawPoint(pen, new Point(cur.X + 4, cur.Y + 1));
+
+        C.DrawPoint(pen, new Point(cur.X, cur.Y + 2));
+        C.DrawPoint(pen, new Point(cur.X + 1, cur.Y + 2));
+        C.DrawPoint(pen, new Point(cur.X + 2, cur.Y + 2));
+        C.DrawPoint(pen, new Point(cur.X + 3, cur.Y + 2));
+
+        C.DrawPoint(pen, new Point(cur.X, cur.Y + 3));
+        C.DrawPoint(pen, new Point(cur.X + 1, cur.Y + 3));
+        C.DrawPoint(pen, new Point(cur.X + 2, cur.Y + 3));
+
+        C.DrawPoint(pen, new Point(cur.X, cur.Y + 4));
+        C.DrawPoint(pen, new Point(cur.X + 1, cur.Y + 4));
+
+        C.DrawPoint(pen, new Point(cur.X, cur.Y + 5));
+        // Mouse Triangle Over
+        
+        //Mouse Tail points are below
+        C.DrawPoint(pen, new Point(cur.X + 3, cur.Y + 3));
+        C.DrawPoint(pen, new Point(cur.X + 4, cur.Y + 4));
+        C.DrawPoint(pen, new Point(cur.X + 5, cur.Y + 5));
+        C.DrawPoint(pen, new Point(cur.X + 6, cur.Y + 6));
+        C.DrawPoint(pen, new Point(cur.X + 7, cur.Y + 7));
     }
     public void RunGUI()
     {
         while (true)
         {
-            C.Clear(Color.White);
-            Pen pen = new Pen(Color.Black);
             // To keep the Mouse within the screen bounds
             if (CMouse.X < 0)
             {
                 CMouse.X = 0;
             }
-            if (CMouse.X > (ScreenWidth - 12))
+            if (CMouse.X > (ScreenWidth - 8))
             {
-                CMouse.X = (uint)(ScreenWidth - 12);
+                CMouse.X = (uint)(ScreenWidth - 8);
             }
             if (CMouse.Y < 0)
             {
                 CMouse.Y = 0;
             }
-            if (CMouse.Y > (ScreenHeight - 20))
+            if (CMouse.Y > (ScreenHeight - 8))
             {
-                CMouse.Y = (uint)(ScreenHeight - 20);
+                CMouse.Y = (uint)(ScreenHeight - 8);
             }
-
             Point cur = new Point((int)CMouse.X, (int)CMouse.Y); //Point where Mouse is currently pointing
+            
+            //Refreshing the screen 
+            int UpdateWidth = 20;
+            int UpdateHeight = 20;
 
-            //To Draw the Mouse Symbol at the Point cur
-            C.DrawLine(pen, cur, new Point(cur.X, cur.Y + 14));
-            C.DrawLine(pen, new Point(cur.X + 1, cur.Y + 1), new Point(cur.X + 10, cur.Y + 10));
-            C.DrawLine(pen, new Point(cur.X + 6, cur.Y + 10), new Point(cur.X + 10, cur.Y + 10));
-            C.DrawLine(pen, new Point(cur.X + 1, cur.Y + 13), new Point(cur.X + 3, cur.Y + 11));
-            C.DrawLine(pen, new Point(cur.X + 4, cur.Y + 12), new Point(cur.X + 6, cur.Y + 17));
-            C.DrawPoint(pen, new Point(cur.X + 6, cur.Y + 11));
-            C.DrawLine(pen, new Point(cur.X + 7, cur.Y + 12), new Point(cur.X + 9, cur.Y + 17));
-            C.DrawLine(pen, new Point(cur.X + 7, cur.Y + 18), new Point(cur.X + 8, cur.Y + 18));
-        }
-    }
-}
-
-
-//Possible Display Driver
-
-//To launch GUI
-//--------------------------------------------------------------------------------------------------------
-/*Console.WriteLine("Booting DisplayDriver.");
-                     try
-                     {
-                         var display = new DisplayDriver();
-                         Console.WriteLine("ATTEMPTING");
-                         display.init(); //init display
-                         display.clear();
-                         display.setPixel((int)40, 50, 60);   
-                     }
-                     catch (Exception)
-                     {
-                         Console.WriteLine("Booting GUI failed. Continue in DOS mode.");
-
-                     }
-                     break;*/
-
-//--------------------------------------------------------------------------------------------------------
-
-
-/*
-public class DisplayDriver
-{
-*
-    protected VGAScreen screen;
-    private int width, height;
-
-    public DisplayDriver()
-    {
-        screen = new VGAScreen();
-    }
-
-    public void init()
-    {
-        screen.SetGraphicsMode(VGAScreen.ScreenSize.Size320x200, VGAScreen.ColorDepth.BitDepth8);
-        width = screen.PixelWidth;
-        height = screen.PixelHeight;
-        clear(0);
-    }
-
-    public virtual void setPixel(int x, int y, int c)
-    {
-        if (screen.GetPixel320x200x8((uint)x, (uint)y) != (uint)c)
-            setPixelRaw(x, y, c);
-    }
-
-    public virtual byte getPixel(int x, int y)
-    {
-        return (byte)screen.GetPixel320x200x8((uint)x, (uint)y);
-    }
-
-    public virtual void clear()
-    {
-        clear(0);
-    }
-    public void DrawFilledRectangle(uint x0, uint y0, int Width, int Height, int color)
-    {
-        for (uint i = 0; i < Width; i++)
-        {
-            for (uint h = 0; h < Height; h++)
+            if( (PrevousMouse.X != cur.X) || (PrevousMouse.Y != cur.Y))
             {
-                setPixel((int)(x0 + i), (int)(y0 + h), color);
+                int x = Math.Min(Math.Max(PrevousMouse.X - (UpdateWidth / 2), 0), (ScreenWidth - UpdateWidth));
+                int y = Math.Min(Math.Max(PrevousMouse.Y - (UpdateHeight / 2), 0), (ScreenHeight - UpdateHeight));
+                Point ClearRectangle = new Point(x, y);
+                C.DrawFilledRectangle(GUIHomePen, ClearRectangle, UpdateWidth, UpdateHeight);
+                PrevousMouse = cur;
             }
+
+            //Draw the Mouse
+            DrawMouse(MousePen, cur);
         }
     }
-    public virtual void clear(int c)
-    {
-        //screen.Clear(c);
-        DrawFilledRectangle(0, 0, width, height, c);
-    }
-
-    public virtual void step() { }
-
-    public int getWidth()
-    {
-        return width;
-    }
-
-    public int getHeight()
-    {
-        return height;
-    }
-
-    public void setPixelRaw(int x, int y, int c)
-    {
-
-        screen.SetPixel320x200x8((uint)x, (uint)y, (uint)c);
-
-    }
-
 }
-*/
+
